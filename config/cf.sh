fb975d6f7238a298beff3e3c5993d2fd0617f9b2
Thịnh <130970460+Rdthaiphno064@users.noreply.github.com>
2025-02-21 13:34:32 +0700

    Update cf.sh

--- config/cf.sh
+++ config/cf.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 check_user_status() {
     local username="$1"
     local user_id=$(curl -s -X POST "https://users.roblox.com/v1/usernames/users" \
         -H "Content-Type: application/json" \
         -d "{\"usernames\": [\"$username\"], \"excludeBannedUsers\": true}" | jq -r '.data[0].id // empty')
+    
     if [[ -n "$user_id" ]]; then
         curl -s -X POST "https://presence.roblox.com/v1/presence/users" \
             -H "Content-Type: application/json" \
@@ -12,4 +14,5 @@ check_user_status() {
         echo "-1"
     fi
 }
+
 check_user_status "$1"
