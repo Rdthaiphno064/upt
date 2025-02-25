5f0b5099430e217cdc0234aea143f4128974a683
Thịnh <130970460+Rdthaiphno064@users.noreply.github.com>
2025-02-25 23:05:17 +0700

    Update Rejoin_.sh

--- config/Rejoin_.sh
+++ config/Rejoin_.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 echo "Loading..."
-if ! command -v sqlite3 >/dev/null 2>&1; then
-    pkg update && pkg upgrade -y && pkg install -y sqlite && pkg install jq >/dev/null 2>&1
+if ! command -v sqlite3 >/dev/null; then
+    pkg update && pkg upgrade -y && pkg install -y sqlite && pkg install jq >/dev/null
 fi
 ROBLOX_PACKAGES=$(pm list packages | grep roblox | cut -d: -f2)
 WEBHOOK_URL2="https://discord.com/api/webhooks/1340266932707917855/dr6Krtq22v1y-YAoosniv2GO5TRyrbK92yh_9Nn30NhRaqK4w3OqZX_vEZOoYTeY2NJJ"
@@ -32,12 +32,8 @@ CONFIG_FILE="$HOME/Downloads/ConfigRejoin.txt"
 if [ -f "$CONFIG_FILE" ]; then
     source "$CONFIG_FILE"
 else
-    GameID="2753915549"
-    TimeRejoin=60
-    RobloxTab=0
-fi
-if [ "$RobloxTab" -gt 0 ] && [ "$RobloxTab" -lt "${#ROBLOX_PACKAGES[@]}" ]; then
-    ROBLOX_PACKAGES=(${ROBLOX_PACKAGES[@]:0:$RobloxTab})
+    GAME_ID="2753915549"
+    TIME_REJOIN=60
 fi
 declare -A LAST_RESTART_TIMES
 for pkg in $ROBLOX_PACKAGES; do
@@ -47,13 +43,13 @@ done
 force_restart() {
     local pkg=$1
     echo "Đóng Roblox Cho $pkg"
-    su -c "am force-stop $pkg" >/dev/null 2>&1
+    su -c "am force-stop $pkg" >/dev/null
     sleep 3
     echo "Mở Roblox Cho $pkg"
-    am start -n ${pkg}/com.roblox.client.startup.ActivitySplash -d "roblox://placeID=${GameID}" >/dev/null 2>&1
+    am start -n ${pkg}/com.roblox.client.startup.ActivitySplash -d "roblox://placeID=${GAME_ID}" >/dev/null
     sleep 10
-    echo "Vào GameID $GameID Cho $pkg"
-    am start -n ${pkg}/com.roblox.client.ActivityProtocolLaunch -d "roblox://placeID=${GameID}" >/dev/null 2>&1
+    echo "Vào GameID $GAME_ID Cho $pkg"
+    am start -n ${pkg}/com.roblox.client.ActivityProtocolLaunch -d "roblox://placeID=${GAME_ID}" >/dev/null
     LAST_RESTART_TIMES[$pkg]=$(date +%s)
 }
 check_and_restart() {
@@ -72,7 +68,7 @@ auto_restart() {
         CURRENT_TIME=$(date +%s)
         for pkg in $ROBLOX_PACKAGES; do
             LAST_RESTART_TIME=${LAST_RESTART_TIMES[$pkg]:-0}
-            if [ $((CURRENT_TIME - LAST_RESTART_TIME)) -ge $((TimeRejoin*60)) ]; then
+            if [ $((CURRENT_TIME - LAST_RESTART_TIME)) -ge $((TIME_REJOIN*60)) ]; then
                 force_restart $pkg
             fi
         done
